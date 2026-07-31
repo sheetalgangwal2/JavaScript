02491713d2dcdbffd90e8fc1b412f6471d034f42
Sheetal Gangwal <sheetalgangwal4@gmail.com>
2026-07-31 22:47:18 +0530

    if-else if -else conditions

---
+++ Conditions/if-else.js
@@ -0,0 +1,28 @@
+//if-else condition
+
+// let age = 12
+
+// if(age>=18){
+//     console.log("Eligible for voting");
+// }
+
+// else{
+//     console.log("not eligible for voting");
+// }
+
+
+
+
+let age = 20;
+
+if(age<18){
+    console.log("kid")
+}
+
+else if(age>=60){
+    console.log("old")
+}
+
+else{
+    console.log("young")
+}
