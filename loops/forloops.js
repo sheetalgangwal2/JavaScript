ca293e49c4941fda61bdb7474556d285c897441f
Sheetal Gangwal <sheetalgangwal4@gmail.com>
2026-07-31 22:46:30 +0530

    for loops and while loops

---
+++ loops/forloops.js
@@ -0,0 +1,16 @@
+//For loops
+
+
+// post increment  
+//counting form 0 to 10
+
+for(let i = 0; i<11; i++){
+    console.log(i)
+}
+
+// post decrement 
+// counting form 1 to 10 
+for ( let i = 10; i>0; i--){
+    console.log(i)
+}
+
---
+++ loops/whileloop.js
@@ -0,0 +1,17 @@
+// Whiel loop 
+let i = 0    //initialize
+
+while(i<10){
+    console.log(i);
+    i++;
+}
+
+
+
+// DO-While loop 
+
+// let f = 0;
+// do{
+//     console.log(i);
+//     f++;
+// }while(f<10);
