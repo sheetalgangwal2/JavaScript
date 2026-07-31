2a064962f3208bc35a666ac9a97e822d5166ac7f
Sheetal Gangwal <sheetalgangwal4@gmail.com>
2026-07-31 22:41:07 +0530

    about different operators

---
+++ operators.js
@@ -0,0 +1,53 @@
+// Operator 
+
+// 1   Arithematic Operator
+
+console.log(2+2);
+console.log(5-3);
+console.log(4/2);
+console.log(2*3);
+console.log(5%2);    //remainder (1)
+console.log(5**2)    //power
+console.log(0/0);    //NaN
+
+
+
+//  2 Assignemet Operators
+
+let x = 20;
+let y = 10;
+// x = x+y;     // x-y    x*y    x/y   x**y    x%y
+
+x+=y;           //x-=y          x*=y.......
+console.log(x);           //30
+
+
+// 3 Comaparison Operators
+
+let a = 10;
+let b = 20;
+
+console.log(a<=b);         //true
+console.log(a>=b);         //false
+console.log(a==b);         //false
+console.log(a===b);         //false
+
+
+
+//Logical operators
+
+console.log(true&&true);   //true
+console.log(true&&false);   //false
+console.log(false&&false);   //false
+
+
+
+// or    agr ek trf bhi true aaya to true show krega
+console.log(true||true);   //true
+console.log(true||false);   //true
+console.log(false||false);   //false
+
+
+// !=      not equal
+console.log(1!=2);     //true  
+console.log(2!=2);     //false
