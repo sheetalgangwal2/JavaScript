07a0600ef54a8b86f22a9a97f70ccfc03aeb4c72
Sheetal Gangwal <sheetalgangwal4@gmail.com>
2026-08-08 22:32:32 +0530

    new concept added

---
+++ filter-reducer/mapping.js
@@ -0,0 +1,9 @@
+//map
+
+const arr =[10, 20, 30, 40, 50, 60];
+
+const newarr = arr.map((num)=>num*2);
+console.log(newarr);              //arr * 2
+
+
+//even about redunce, accumulate, price adding ,index and more 
